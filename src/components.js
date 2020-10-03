aa74773a38e728ef00c01832af3c7b40e157bac7
Michael Lange <dingoeatingfuzz@gmail.com>
2020-10-03 16:31:19 -0700

    Revise the Tile component given the new pathfinding idea

--- src/components.js
+++ src/components.js
@@ -4,9 +4,7 @@ export class Tile extends Component {};
 Tile.schema = {
   x: { type: Types.Number },
   y: { type: Types.Number },
-  up: { type: Types.Boolean },
-  right: { type: Types.Boolean },
-  down: { type: Types.Boolean },
-  left: { type: Types.Boolean },
-  occupied: { type: Types.Boolean },
+  id: { type: Types.String },
+  isOccupied: { type: Types.Boolean },
+}
 }
