ab0243637c28f12ed0fb22b9f280b124a601e869
Nick <naccuardi@gmail.com>
2020-10-03 21:29:11 -0700

    Abiility to add new towers

--- src/components.js
+++ src/components.js
@@ -21,3 +21,13 @@ export class Phaser extends Component {};
 Phaser.schema = {
   game: { type: Types.Ref },
 }
+
+export class Tower extends Component {};
+Tower.schema = {
+  x:            { type: Types.Number },
+  y:            { type: Types.Number },
+  damage:       { type: Types.Number  },
+  range:        { type: Types.Number  },
+  rateOfFire:   { type: Types.Number  },
+  areaOfEffect: { type: Types.Boolean },
+}
