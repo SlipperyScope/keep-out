c2c040a25b34fd3501559bf17e629a382f92e42c
Nick Accuardi <naccuardi@gmail.com>
2020-10-04 13:45:30 -0700

    removed commented out code

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,16 +11,6 @@ export default class Menu extends Phaser.Scene {
   }
 
   create() {
-    // const logo = this.add.image(640, 150, "logo");
-
-    // this.tweens.add({
-    //   targets: logo,
-    //   y: 450,
-    //   duration: 1000,
-    //   ease: "Power2",
-    //   yoyo: true,
-    //   loop: -1
-    // });
 
     const spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     spaceKey.on("down", () => {
