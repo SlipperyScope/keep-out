31422c99f5b4eba7e3219ef04e8a3d62b2ce37ca
Michael Lange <dingoeatingfuzz@gmail.com>
2020-10-05 12:44:01 -0700

    Add health to the play scene

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -7,9 +7,37 @@ export default class Play extends SuperScene {
     super.create();
     this.game.world.createEntity().addComponent(EnemyEmitter, { isRunning: true, remaining: 30, releaseRate: 10 });
 
+    this.hud = new Hud(this, 50, this.game.config.height - 100);
+
     const spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     spaceKey.on("down", () => {
       this.scene.switch("prep");
     });
+  }
+}
+
+class Hud extends Phaser.GameObjects.Container {
+  constructor(scene) {
+    super(...arguments);
+    this.depth = 9999;
+
+    this.text = scene.add.text(0, 0, 'XX', {
+      fontFamily: 'sans-serif',
+      fontSize: 28,
+      color: '#F00',
+      shadow: {
+        offsetX: 2,
+        offsetY: 2,
+        color: '#000',
+        blur: 1,
+      }
+    });
+
+    this.add(this.text);
+    scene.add.existing(this);
+  }
+
+  setHealth(health) {
+    this.text.text = health;
   }
 }
